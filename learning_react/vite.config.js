cdbad694fd497e6c418161f092117385c24d83a6
RaulEstevezA <tevezabella@hotmail.com>
2025-01-25 14:39:05 +0100

    pruebas

--- learning_react/vite.config.js
+++ learning_react/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/paradise",
+    base: "/learning_react/",
   plugins: [react()],
 })
